9490975d3e6176dc037304c049d422d2c5135d04
Tovisi Zoltan <tovisi.zoltan@student.ms.sapientia.ro>
2022-10-30 23:35:15 +0200

    Bottom Navigation Started

--- Android/Quizapp/app/src/main/java/com/example/quizapp/MainActivity.kt
+++ Android/Quizapp/app/src/main/java/com/example/quizapp/MainActivity.kt
@@ -5,9 +5,16 @@ import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
+import androidx.fragment.app.Fragment
+import com.example.quizapp.databinding.ActivityMainBinding
+import com.example.quizapp.databinding.FragmentHomeBinding
+import com.example.quizapp.models.item
+import com.example.quizapp.ui.*
 
+
+@Suppress("UNUSED_EXPRESSION")
 class MainActivity : AppCompatActivity() {
-
+    private lateinit var binding: ActivityMainBinding;
     val TAG = "MainActivity"
 
     override fun onStart() {
@@ -42,7 +49,31 @@ class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        //replaceFragment(HomeFragment())
         Log.i(TAG, "onCreate() called.")
+
+        binding.bottomNavigationView.setOnItemSelectedListener {
+            when(it.itemId){
+                R.id.home -> replaceFragment(HomeFragment())
+                R.id.quizTime -> replaceFragment(QuizStartFragment())
+                R.id.profile -> replaceFragment(ProfileFragment())
+                R.id.listOfQuestions -> replaceFragment(QuestionListFragment())
+                R.id.newQuestion -> replaceFragment(QuestionAddFragment())
+
+                else -> {
+
+                }
+            }
+            true
+        }
+    }
+
+    private fun replaceFragment(fragment: Fragment){
+        val fragmentManager = supportFragmentManager
+        val fragmentTransaction = fragmentManager.beginTransaction()
+        fragmentTransaction.replace(R.id.frameLayout, fragment)
+        fragmentTransaction.commit()
     }
 }
---
+++ Android/Quizapp/app/src/main/java/com/example/quizapp/ui/HomeFragment.kt
@@ -0,0 +1,29 @@
+package com.example.quizapp.ui
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.example.quizapp.R
+import com.example.quizapp.databinding.FragmentHomeBinding
+
+
+class HomeFragment : Fragment() {
+    private lateinit var binding: FragmentHomeBinding
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        arguments?.let {
+
+        }
+    }
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+
+        return inflater.inflate(R.layout.fragment_home, container, false)
+    }
+
+}
